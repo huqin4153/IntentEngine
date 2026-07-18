7cd9ac1ce7c3f985e278eb2a67179a2e597eebc4
huqin4153 <huqin4153@gmail.com>
2026-07-18 08:44:03 +0800

    Add files via upload

---
+++ IntentEngine/Properties/PublishProfiles/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IntentEngine")]
+[assembly: AssemblyDescription("意图驱动的企业业务查询系统")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IntentEngine")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("A1B2C3D4-E5F6-7890-ABCD-EF1234567890")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
